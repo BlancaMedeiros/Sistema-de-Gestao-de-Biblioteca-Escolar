41e643ba0dd91a09e6ecd65e02163fac7ad7c83e
BlancaMedeiros <biancamedeiroslabiapari@gmail.com>
2026-09-02 18:36:09 -0300

    Criação do menu de navegação do cabeçalho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+//import { DashboardComponent } from './pages/dashboard/dashboard.component';
+//import { AcervoComponent } from './pages/acervo/acervo.component';
+//import { EmprestimosComponent } from './pages/emprestimos/emprestimos.component';
+//import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // Se entrar sem caminho, vai pro dashboard
+ // { path: 'dashboard', component: DashboardComponent },
+ // { path: 'acervo', component: AcervoComponent },
+ // { path: 'emprestimos', component: EmprestimosComponent },
+ // { path: 'usuarios', component: UsuariosComponent }
+];
